090b20bab2dec961a84b671890f47a6f41890457
Lisa Marie Goicochea <lisamgoicochea@gmail.com>
2018-01-30 10:27:25 -0500

    necessary changes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
 // Enemies our player must avoid
-var Enemy = function(startX, startY, speed) {
+var Enemy = function (x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = startX;
-    this.y = startY;
+    this.x = x;
+    this.y = y;
     this.speed = speed;
-    this.height = 44;
-    this.width = 64;
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,14 +13,23 @@ var Enemy = function(startX, startY, speed) {
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    this.x += this.speed * dt; // multiplication by the dt parameter, same speed for all computers
+Enemy.prototype.update = function (dt) {
+    this.x += this.speed * dt;// multiplication by the dt parameter, same speed for all computers
+    
+
     if (this.x > 480) { // if and when a bug reaches the right edge
-        this.y = -50; // new bug appears on left edge
-        this.speed = randomSpeed();
-    } 
-      this.checkCollision();
-    };
+        this.x = -123; // new bug appears on left edge
+        this.speed = 100 + Math.floor(Math.random() * 484);
+    }
+
+  if (player.x < this.x + 60 &&
+      player.x + 27 > this.x &&
+      player.y < this.y + 25 &&
+      30 + player.y > this.y) {
+      player.x = 204;
+      player.y = 404;
+  }
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -31,22 +39,37 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 //This class requires and update(), render() and a handleInput() method.
 var Player = function(x, y, speed) {
-    this.x = startX;
-    this.y = startY;
-    this.height = 88;
-    this.width = 44;
+    this.x = x;
+    this.y = y;
     this.sprite = 'images/char-princess-girl.png';
+    this.speed = speed;
 };
 
 // This class requires an update(), render() and
 // a handleInput() method.
 
 // when reaching water, colliding with bug, collecting a star
-Player.prototype.update = function() { // UPDATE
+Player.prototype.update = function (dt) { // UPDATE
     'use strict';
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
+if (this.y > 384) {
+    this.y = 384;
+}
+
+if (this.x > 404) {
+    this.x = 400;
+}
+
+if (this.x < 0) {
+    this.x = 0;
+}
+
+if (this.y < 0) {
+  this.x = 204;
+  this.y = 284;
+}
 // player is drawn on the screen
 Player.prototype.render = function() { // RENDER
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -74,18 +97,18 @@ Player.prototype.handleInput = function (key) { // HANDLEINPUT
 
 if (this.x > 398) {
   this.x = 398;
-};
+}
 if (this.y > 434) {
   this.y = 435;
-};
+}
 
 if (this.x < 0) {
   this.x = 0;
-};
+}
 if (this.y < 0) {
-  alert("You've Made it!")
+  alert("You've Made it!");
   this.reset();
-  };
+  }
 };
 
 Enemy.prototype.checkCollision = function() {
@@ -95,13 +118,13 @@ Enemy.prototype.checkCollision = function() {
     player.y + player.height > this.y &&
     this.height + this.y > player.y) {
     player.reset();
-  };
+  }
 };
 
 Player.prototype.reset = function() {
   'use strict';
-  this.x = 200;
-  this.y = 414;
+  this.x = 202;
+  this.y = 404;
 };
 
 // Now instantiate your objects.
@@ -114,8 +137,10 @@ var allEnemies = [
 ];
 
 // Place the player object in a variable called player
-
 var player = new Player(202,388);
+
+var enemyPosition = [50, 150, 210];
+var enemy;
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
